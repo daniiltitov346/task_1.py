1271667fc688b46990b2a00a9a870c933c6ba3b4
Selectel Git User <git-user@selectel.ru>
2025-09-21 17:48:13 +0300

    delete

--- task_1.py
+++
@@ -1,8 +0,0 @@
-fio = input("Введите ФИО: ")
-space1 = fio.find(" ")
-space2 = fio.find(" ", space1 + 1)
-surname = fio[:space1]
-name_letter = fio[space1 + 1]
-patronymic_letter = fio[space2 + 1]
-
-print(surname, name_letter + "." + patronymic_letter + ".")
